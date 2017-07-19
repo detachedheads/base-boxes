65a56cd52048060519330a7ddfe820954a7adefc
Anthony Spring <anthonyspring@gmail.com>
2017-07-19 00:59:55 -0400

    Add the initial pass at the ubuntu 16.04 preseed and template.

---
+++ templates/ubuntu-16.04-virtualbox.rb
@@ -0,0 +1,35 @@
+BOX_OS          = 'ubuntu-16.04'
+BOX_OS_SHORT    = 'ubuntu'
+BOX_BUILDER     = 'virtualbox'
+BOX_NAME        = File.basename(__FILE__, '.rb') 
+
+USER_VARIABLES        = {
+  'ansible_playbook' => 'bits/ansible/playbooks/vagrant-box/main.yml',
+}
+
+TEMPLATE_DEFINITIONS  = [
+  # Variables
+  File.join(VARIABLES_TEMPLATES,        "os/#{BOX_OS}/general.rb"),
+
+  # # Builders
+  File.join(BUILDER_TEMPLATES,          "#{BOX_BUILDER}/iso.rb"),           # Bring in the virtualbox iso configuration
+  File.join(OS_TEMPLATES,               "#{BOX_OS}/#{BOX_BUILDER}/iso.rb"), # Bring in the centos iso configuration
+
+  # Provisioner - Upgrade all packages
+  File.join(PROVISIONER_TEMPLATES,      "os/#{BOX_OS_SHORT}/apt-upgrade.rb"),
+
+  # Provisioner - Install Ansible
+  File.join(PROVISIONER_TEMPLATES,      "os/#{BOX_OS_SHORT}/install-ansible.rb"),
+  
+  # Provisioner - Ansible Provisioning
+  File.join(PROVISIONER_TEMPLATES,      'ansible/local-playbook.rb'),
+
+  # Provisioner - Uninstall Ansible
+  File.join(PROVISIONER_TEMPLATES,      "os/#{BOX_OS_SHORT}/uninstall-ansible.rb"),
+  
+  # Provisioner - Cleanup
+  File.join(PROVISIONER_TEMPLATES,      'os/linux/zero-empty-space.rb'),
+  
+  # Post Processors
+  File.join(POSTPROCESSOR_TEMPLATES,    'vagrant/output.rb'),       # Configure the vagrant box output
+]
